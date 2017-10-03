11a5fbbc151467ad8c5908b31c8c2dec0abd0522
cloud-elric <humberto@2gom.com.mx>
2017-10-03 14:23:23 -0500

    Integracion de pantalla de seleccionar

--- views/site/index.php
+++ views/site/index.php
@@ -10,32 +10,63 @@ $this->registerJsFile(
     ['depends' => [\yii\web\JqueryAsset::className()]]
 );
 ?>
-
-<div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <div class="panel">
-            <div class="panel-body">
-                <?php $form = ActiveForm::begin(); ?>
-
-                <a class="btn btn-primary btn-block js-select-amount <?=$ordenCompra->num_total==100?'btn-success':''?>"  data-value="100">$ 100 MXN</a>
-
-                <a class="btn btn-primary btn-block js-select-amount <?=$ordenCompra->num_total==200?'btn-success':''?>" data-value="200">$ 200 MXN</a>
-
-                <a class="btn btn-primary btn-block js-select-amount <?=$ordenCompra->num_total==300?'btn-success':''?>" data-value="300">$ 300 MXN</a>
-
-                <div class="form-group">
-                    <label for="amount">Otra cantidad</label>
-                    <input id="amount" type="text" class="form-control js-add" value="0" />
-                </div>
-
-                <?= $form->field($ordenCompra, 'num_total')->hiddenInput(['maxlength' => true])->label(false)?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Donar', ['class' => 'btn btn-success']) ?>
-                </div>
-
-                <?php ActiveForm::end(); ?>
-            </div>
+<section class="donativos-wrapper">
+    <div class="container container-full">
+      <div class="donativos-content">
+        <h3 class="title">Elige el monto con el cual desees colaborar</h3>
+        <div class="tarjetas-wrapper">
+          <div class="tarjeta">
+            <div class="header">Donar</div>
+            <div class="monto">
+              <span class="currency">$</span>
+              <span class="cantidad">100</span>
+              <span class="moneda">mxn</span></div>
+            <a  class="btn btn-default btn-donativo js-select-amount <?=$ordenCompra->num_total==100?'btn-success':''?>"  data-value="100">Realizar Donativo</a>
+          </div>
+          <div class="tarjeta">
+            <div class="header">Donar</div>
+            <div class="monto">
+              <span class="currency">$</span>
+              <span class="cantidad">200</span>
+              <span class="moneda">mxn</span></div>
+            <a class="btn btn-default btn-donativo js-select-amount <?=$ordenCompra->num_total==200?'btn-success':''?>"  data-value="200">Realizar Donativo</a>
+          </div>
+          <div class="tarjeta">
+            <div class="header">Donar</div>
+            <div class="monto">
+              <span class="currency">$</span>
+              <span class="cantidad">500</span>
+              <span class="moneda">mxn</span></div>
+            <a   class="btn btn-default btn-donativo js-select-amount <?=$ordenCompra->num_total==500?'btn-success':''?>"  data-value="200">Realizar Donativo</a>
+          </div>
+          <div class="tarjeta">
+            <div class="header">Donar</div>
+            <div class="monto">
+              <span class="currency">$</span>
+              <span class="cantidad">900</span>
+              <span class="moneda">mxn</span></div>
+            <a   class="btn btn-default btn-donativo js-select-amount <?=$ordenCompra->num_total==900?'btn-success':''?>"  data-value="900">Realizar Donativo</a>
+          </div>
         </div>
+        <div class="custom-amount-wrapper">
+          <h3>¿ Tienes otro número en mente ?</h3>
+          <div class="custom-bar">
+            <div class="header">Donar</div>
+            <div class="monto">
+              <span class="currency">$</span>
+              <?php $form = ActiveForm::begin(); ?>
+
+                <input id="amount" type="text" placeholder="100.00">
+
+                
+              <span class="moneda">mxn</span>
+            </div>
+            <a href="" class="btn btn-default btn-donativo submit-btn">Realizar Donativo</a>
+            <?= $form->field($ordenCompra, 'num_total')->hiddenInput(['maxlength' => true])->label(false)?>
+            <?php ActiveForm::end(); ?>
+          </div>
+
+        </div>
+      </div>
     </div>
-</div>
+  </section>            
