d85f81e840fc27c0603f82073bd41d913a52616b
cloud-elric <humberto@2gom.com.mx>
2017-10-03 14:30:45 -0500

    Correccion de main

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,55 +28,8 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'Donativos',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            !Yii::$app->user->isGuest ? (
-                ['label' => 'Mis boletos', 'url' => ['/site/mis-boletos']]
-            ) : '',
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->txt_username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-                ),
-
-        ],
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
         <?= $content ?>
-    </div>
-</div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; Nombre compañia <?= date('Y') ?></p>
-
-        <p class="pull-right">Powered by <a href="www.2gom.com.mx">2 Geeks one Monkey</a></p>
-    </div>
-</footer>
+    
 
 <?php $this->endBody() ?>
 </body>
