af8ae476716862af649c69c171c674d91a5465d0
cloud-elric <humberto@2gom.com.mx>
2017-10-03 17:17:26 -0500

    SE agregaron los boletos

--- views/site/mis-boletos.php
+++ views/site/mis-boletos.php
@@ -1,10 +1,26 @@
+<section class="donativos-wrapper">
+    <div class="container container-column container-full">
+      <div class="boletos-content">
+        <h3>Estos son los boletos con los que podrás participar en la rifa</h3>
+        <?php
+        foreach($boletos as $boleto){
+        ?>
+        <div class="boletos">
+          <div class="boleto">
+            <h2> <?=$boleto->txt_codigo?></h2>
+          </div>
+        </div>
+        <?php
+        }
+        ?>
+      </div>
+    </div>
+</section>
 
-<?php
-foreach($boletos as $boleto){
-?>
 
-                <?=$boleto->txt_codigo?>
+
+
+
                
-<?php
-}
-?>
+               
+
