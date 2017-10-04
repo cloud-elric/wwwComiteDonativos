4fe79f404d4344cfc4bdf584c003d185330c9b10
cloud-elric <humberto@2gom.com.mx>
2017-10-04 18:57:08 -0500

    Se agrego loader

--- views/site/procesando-pago.php
+++ views/site/procesando-pago.php
@@ -1,7 +1,22 @@
+<style>
+.loader {
+    border: 16px solid #f3f3f3; /* Light grey */
+    border-top: 16px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+</style>
 <section class="donativos-wrapper">
     <div class="container-full">
         <div class="login-content">
-            <h3>Pago en proceso</h3>
+            <h3><div class="loader"></div>Pago en proceso</h3>
         </div>
     </div>
   </section>
