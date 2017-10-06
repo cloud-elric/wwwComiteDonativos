6b3158c50b9822b9091ea1566ff42f366c9dcf95
cloud-elric <humberto@2gom.com.mx>
2017-10-06 11:48:20 -0500

    Correcion de nombre de imagen

--- views/layouts/mainNoHeader.php
+++ views/layouts/mainNoHeader.php
@@ -49,7 +49,7 @@ AppAsset::register($this);
 
     <footer>
       <a class="sponsor" href="http://www.2geeksonemonkey.com">Desarrollo donado por 2 Geeks one Monkey</a>
-      <img src="webassets/images/2geeks-isotipo.png" alt="Desarrollo donado por 2 Geeks one Monkey">
+      <img src="webassets/images/2Geeks-isotipo.png" alt="Desarrollo donado por 2 Geeks one Monkey">
     </footer>
   </section>
 
